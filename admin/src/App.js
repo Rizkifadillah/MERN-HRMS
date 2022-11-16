b3b0a120bdc72d74ca4cbadffdbf7636df1afd47
RizkiFadillah <rizkifadillah26.rf@gmail.com>
2022-11-16 22:46:10 +0700

    sidebar navlink

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>React</h1>
+        <Router>
+          <Routes>
+            <Route path="*" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
---
+++ admin/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import Sidebar from '../components/Sidebar';
+import Top from '../components/Top';
+import Activities from './mainpages/Activities';
+import Dashboard from './mainpages/Dashboard';
+import Department from './mainpages/Department';
+import Employee from './mainpages/Employee';
+import Events from './mainpages/Events';
+import User from './mainpages/User';
+import Holidays from './mainpages/Holidays'
+import Accounts from './mainpages/Accounts';
+
+const Home = () => {
+  return (
+    <div className="container">
+        <div className="row">
+            <Sidebar />
+        </div>
+        <div className="row">
+            <div className="col">
+                <Top/>
+            </div>
+            <div className="col">
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/users" element={<User />} />
+                    <Route path="/departments" element={<Department />} />
+                    <Route path="/employee" element={<Employee />} />
+                    <Route path="/activities" element={<Activities />} />
+                    <Route path="/events" element={<Events />} />
+                    <Route path="/holidays" element={<Holidays />} />
+                    <Route path="/accounts" element={<Accounts />} />
+                </Routes>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
